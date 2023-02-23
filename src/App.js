1637fdc79ff3a429d157a6e2f756b7ae4e4abc00
Cameron Flowers <cameron.flowers13@gmail.com>
2023-02-22 18:39:01 -0600

    example App.js change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Adding a H1 to our site! 😮‍💨</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Yoooo i'm hacking in React.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,7 +18,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
